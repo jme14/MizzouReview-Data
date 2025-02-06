db54786f1c4ae5f0ba876770853a80fb0fd6b2a3
jme14 <williamsltrey3@gmail.com>
2025-02-06 12:49:56 -0600

    add first drafts for tests

--- test/database.test.ts
+++ test/database.test.ts
@@ -1,4 +1,13 @@
 import {expect, test} from "vitest"
+import { Firestore } from "firebase-admin/firestore";
 import { writeProfessor } from "../src/database";
+import {Professor} from "../src/models/professor"
 
-test("testing that this is true", ()=> true)
+test("writeProfessor", ()=> {
+    const badDB = new Firestore()
+    const professor = new Professor("jimr")
+    writeProfessor(badDB, professor)
+})
+test("writeProfessor -- Overwriting via optional paramters", ()=> {
+    expect(false).toBe(true)
+})
