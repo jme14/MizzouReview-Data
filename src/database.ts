d682dfeea440ef58df96c9d8c239985f49137b63
jme14 <williamsltrey3@gmail.com>
2025-02-06 11:34:45 -0600

    rename functions for style

--- src/database.ts
+++ src/database.ts
@@ -1,55 +1,58 @@
-import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
 import {
-  getFirestore,
-  Firestore,
-  Timestamp,
-  FieldValue,
-  Filter,
-  CollectionReference,
-  DocumentReference,
-} from "firebase-admin/firestore";
+    getFirestore,
+    Firestore,
+    Timestamp,
+    FieldValue,
+    Filter,
+    CollectionReference,
+    DocumentReference,
+} from 'firebase-admin/firestore';
 
 import {
-  ObjectiveMetrics,
-  SubjectiveMetrics,
-  BasicInfo,
-  Professor,
-  AIPromptAnswers,
-} from  "./models/professor"
+    ObjectiveMetrics,
+    SubjectiveMetrics,
+    BasicInfo,
+    Professor,
+    AIPromptAnswers,
+} from './models/professor';
 
-const firebaseConfig = {
-  credential: cert(require("../keys/admin.json")),
-};
+export async function getProfessorFromID(
+    db: Firestore,
+    professorId: string,
+): Promise<Professor | null> {
+    const docRef = db.collection('professors').doc(professorId);
+    const docSnapshot = await docRef.get();
 
-const app = initializeApp(firebaseConfig);
-const db: Firestore = getFirestore(app);
-
-export async function getProfessorDocumentFromID(professorId: string) {
-  const docRef = db.collection("professors").doc(professorId);
-  const docSnapshot = await docRef.get();
-
-  if (docSnapshot.exists) {
-    console.log("FOUND IT!");
-  } else {
-    console.log("Didn't find it :( ");
-  }
+    if (docSnapshot.exists) {
+        console.log('FOUND IT!');
+        return Professor.initFromObject(docSnapshot.data());
+    } else {
+        console.log("Didn't find it :( ");
+        return null;
+    }
 }
 
 // note: the name needs to be exact ;-;
-export async function getProfessorsFromDepartment(department: string): Promise<Professor[]>{
-  const queryResult = await db.collection("professors").where("basicInfo.department", "==", department).get()
-  const professors: Professor[] = []
-  queryResult.forEach((doc) => {
-    professors.push(Professor.initFromObject(doc.data()))
-  })
-  return professors
+export async function getProfessorsFromDepartment(
+    db: Firestore,
+    department: string,
+): Promise<Professor[]> {
+    const queryResult = await db
+        .collection('professors')
+        .where('basicInfo.department', '==', department)
+        .get();
+    const professors: Professor[] = [];
+    queryResult.forEach((doc) => {
+        professors.push(Professor.initFromObject(doc.data()));
+    });
+    return professors;
 }
 
 // enable overwrite if you expect that the professor already exists in the database.
-export async function writeProfessor(professor: Professor) {
-  const docRef = db.collection("professors").doc(professor.professorId);
+export async function writeProfessor(db: Firestore, professor: Professor) {
+    const docRef = db.collection('professors').doc(professor.professorId);
 
-  const professorThatCanWrite = JSON.parse(JSON.stringify(professor)) 
-  await docRef.set(professorThatCanWrite);
-  console.log("Done!?");
+    const professorThatCanWrite = JSON.parse(JSON.stringify(professor));
+    await docRef.set(professorThatCanWrite);
+    console.log('Done!?');
 }
