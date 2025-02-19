555b10535278778f266060108fbc1717e5431ef8
jme14 <williamsltrey3@gmail.com>
2025-02-19 13:48:08 -0600

    add commenting

--- src/database.ts
+++ src/database.ts
@@ -16,6 +16,12 @@ import {
     AIPromptAnswers,
 } from './models/professor';
 
+/**
+ * 
+ * @param {Firestore} db 
+ * @param {string} professorId 
+ * @returns  {Promise<Professor | null>}
+ */
 export async function getProfessorFromID(
     db: Firestore,
     professorId: string,
@@ -24,23 +30,31 @@ export async function getProfessorFromID(
     const docSnapshot = await docRef.get();
 
     if (docSnapshot.exists) {
-        console.log('FOUND IT!');
         return Professor.initFromObject(docSnapshot.data());
     } else {
-        console.log("Didn't find it :( ");
         return null;
     }
 }
 
-// note: the name needs to be exact ;-;
+
+/**
+ * 
+ * @param {Firestore} db 
+ * @param {string} department 
+ * @returns {Promise<Professor[]>}
+ */
 export async function getProfessorsFromDepartment(
     db: Firestore,
     department: string,
 ): Promise<Professor[]> {
+
+    // find all professors with the same dept as that passed 
     const queryResult = await db
         .collection('professors')
         .where('basicInfo.department', '==', department)
         .get();
+
+    // make a new professor object for all results 
     const professors: Professor[] = [];
     queryResult.forEach((doc) => {
         professors.push(Professor.initFromObject(doc.data()));
@@ -49,10 +63,38 @@ export async function getProfessorsFromDepartment(
 }
 
 // enable overwrite if you expect that the professor already exists in the database.
+/**
+ * 
+ * @param {Firestore} db 
+ * @param {Professor} professor 
+ */
 export async function writeProfessor(db: Firestore, professor: Professor) {
+    // get a professor object doc referrence from the professor.id
     const docRef = db.collection('professors').doc(professor.professorId);
 
+    // convert professor object into JSON object
     const professorThatCanWrite = JSON.parse(JSON.stringify(professor));
+    
+    // write the JSON object into the doc reference 
     await docRef.set(professorThatCanWrite);
     console.log('Done!?');
+}
+
+/**
+ * Use this for new professors
+ * @param db 
+ * @param professor 
+ * @returns 
+ */
+export async function initializeProfessor(db: Firestore, professor: Professor){
+    return false
+}
+
+/**
+ * Use this to update an existing professor record 
+ * @param db 
+ * @param professor 
+ */
+export async function updateProfessor(db: Firestore, professor: Professor){
+
 }
