d56b937580134610f3d3c3e74c9add2c5fd8af47
jme14 <williamsltrey3@gmail.com>
2025-02-23 10:24:34 -0600

    almost finished with mucourses testing

--- src/mucourses.ts
+++ src/mucourses.ts
@@ -1,12 +1,61 @@
 // this file is for interacting with the mucourses api 
 import axios from "axios"
 
-export async function getCourses() {
+export type mucoursesData = {
+    dept: string;
+    title: string;
+    number: string;
+    section: string;
+    term: string;
+    instructor: string;
+    arange: number;
+    brange: number;
+    crange: number;
+    drange: number;
+    frange: number;
+    average: number;
+}
+
+// will eventually want to not export this function
+export async function getCourses(): Promise<mucoursesData[]> {
     try {
         const { data } = await axios.get('https://mucourses.com/api/courses');
         return data
     } catch (error) {
         console.error('Error fetching courses:', error);
     }
+    return []
 }
 
+export async function getCoursesByProfessor(fname: string, lname: string, mname?: string):Promise<mucoursesData[]>{
+    
+    let allCourses = await getCourses() 
+
+    const professorNameStringNoMiddle = `${lname},${fname}`
+    const professorNameStringMiddleInitial = `${lname},${fname} ${mname?.slice(0,1)}`
+    const professorNameStringMiddleName = `${lname},${fname} ${mname}`
+
+    const potentialNames = [professorNameStringMiddleInitial, professorNameStringMiddleName, professorNameStringNoMiddle]
+    const results: mucoursesData[] = []
+
+    function someMiddleInitial(str: string): boolean {
+        const pattern = /${lname},${fname} [A-Z]/
+        return pattern.test(str)
+    }
+    
+    // might be able to speed up
+    allCourses.forEach((course: mucoursesData) => {
+        if(potentialNames.includes(course.instructor)){
+            results.push(course)
+        }
+        else if (someMiddleInitial(course.instructor)){
+            results.push(course)
+        }
+    })
+
+    if (results.length != 0){
+        return results
+    }
+
+
+}
--- test/mucourses.test.ts
+++ test/mucourses.test.ts
@@ -1,7 +1,42 @@
-import {expect, test} from "vitest"
-import {getCourses} from "../src/mucourses"
+import {describe, expect, test} from "vitest"
+import {mucoursesData, getCourses, getCoursesByProfessor} from "../src/mucourses"
 
-test("getCourses", async ()=> {
-    const data = await getCourses()
-    console.log(data)
+/*
+describe("API access", () => {
+    test("getCourses defined", async ()=> {
+        const data = await getCourses()
+        expect(data).toBeDefined()
+    })
+
+    test("getCourses consistent object subset test ", async () => {
+        const data: Object[] = await getCourses()
+        
+        const keySet = new Set()
+        Object.keys(data[0]).forEach((key) => keySet.add(key))
+        
+        const dataSubset = data.slice(0,100)
+        dataSubset.forEach((datapoint: Object) => {
+            Object.keys(datapoint).forEach((key) => {
+                console.log(keySet)
+                console.log(key)
+                expect(keySet.has(key)).toBe(true)
+            })
+        })
+        console.log(keySet)
+    })
+})
+*/
+describe("API result filtering", () => {
+    test("getCourses by instructor name, known", async() => {
+        const results: mucoursesData[] = await getCoursesByProfessor("Jill", "Moreland", "Annette")
+        expect(results.length).toBeGreaterThan(0)
+        console.log(results)
+    })
+
+    test("getCourses by instructor name, unknown", async() => {
+        const results: mucoursesData[] = await getCoursesByProfessor("Ries", "James")
+        expect(results.length).toBeGreaterThan(0)
+        console.log(results)
+
+    })
 })
