c69919319124b9d75516a8921ab26bae86f1c1f4
jme14 <williamsltrey3@gmail.com>
2025-02-26 15:11:41 -0600

    add name module for easier name handling

---
+++ src/models/name.ts
@@ -0,0 +1,45 @@
+export class Name {
+    fname: string;
+    mname?: string[];
+    lname: string;
+
+    constructor(fname: string, lname: string, mname?: string[]) {
+        this.fname = fname;
+        this.lname = lname;
+        if (mname) {
+            this.mname = mname;
+        }
+    }
+
+    static getNameFromString(strToFormat: string, formatting: string): Name {
+        let pattern = formatting
+            .replace('{fname}', '(?<fname>\\w+)')
+            // remove the white space around mname if it exists
+            // then, on right, optionally replace with formatter that includes any amount of words with spaces
+            .replace(/\s*\{mname\}\s*/, '(?:\\s+(?<mname>[\\w\\s]+))?\\s+')
+            .replace('{lname}', '(?<lname>\\w+)');
+
+        let regex = new RegExp(`^${pattern}$`);
+        let match = strToFormat.match(regex);
+
+        if (!match || !match.groups) {
+            // TODO: fix this stupidity lol 
+            if (strToFormat.endsWith(' ')) {
+                throw new Error(
+                    `String '${strToFormat}' does not match format '${formatting}'`,
+                );
+            } else {
+                return Name.getNameFromString(strToFormat + " ", formatting)
+            }
+        }
+
+        if (match.groups.mname) {
+            return new Name(
+                match.groups.fname,
+                match.groups.lname,
+                match.groups.mname.split(' '),
+            );
+        }
+        return new Name(match.groups.fname, match.groups.lname);
+    }
+}
---
+++ test/models/name.test.ts
@@ -0,0 +1,46 @@
+import { expect, test, describe } from 'vitest';
+import { Name } from '../../src/models/name';
+
+describe('Testing name creation', () => {
+    test('testing name creation', () => {
+        const myName = new Name('Trey', 'Williams');
+    });
+});
+describe('Testing static name creation', () => {
+    test('{fname} {lname}', () => {
+        const exampleName = 'Jim Ries';
+        const myName = Name.getNameFromString(exampleName, '{fname} {lname}');
+        expect(myName).toStrictEqual(new Name("Jim", "Ries"))
+    });
+    test('{fname} {mname} {lname}', () => {
+        const exampleName = 'Jim E Ries';
+        const myName = Name.getNameFromString(exampleName, '{fname} {mname} {lname}');
+        expect(myName).toStrictEqual(new Name("Jim", "Ries", ["E"]))
+    });
+    test('{fname} {mname} {lname} with no middle name ', () => {
+        const exampleName = 'Jim Ries';
+        const myName = Name.getNameFromString(exampleName, '{fname} {mname} {lname}');
+        expect(myName).toStrictEqual(new Name("Jim", "Ries"))
+    });
+    test('{lname}, {fname}', () => {
+        const exampleName = 'Ries, Jim';
+        const myName = Name.getNameFromString(exampleName, '{lname}, {fname}');
+        expect(myName).toStrictEqual(new Name("Jim", "Ries"))
+    });
+    test('{lname}, {fname} {mname}', () => {
+        const exampleName = 'Ries, Jim E';
+        const myName = Name.getNameFromString(exampleName, '{lname}, {fname} {mname}');
+        expect(myName).toStrictEqual(new Name("Jim", "Ries", ["E"]))
+    });
+    test('{lname}, {fname} {mname} with more than 1 middle name', () => {
+        const exampleName = 'Ries, Jim Edward Smitty';
+        const myName = Name.getNameFromString(exampleName, '{lname}, {fname} {mname}');
+        expect(myName).toStrictEqual(new Name("Jim", "Ries", ["Edward", "Smitty"]))
+    });
+    test('{lname}, {fname} {mname} with no middle names', () => {
+        const exampleName = 'Ries, Jim';
+        const myName = Name.getNameFromString(exampleName, '{lname}, {fname} {mname}');
+        expect(myName).toStrictEqual(new Name("Jim", "Ries"))
+    });
+
+});
