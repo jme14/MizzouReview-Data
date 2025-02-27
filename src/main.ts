0f9463693ef273bc47bb4e7cb8a3d8afc98d5a98
jme14 <williamsltrey3@gmail.com>
2025-02-27 12:39:12 -0600

    onProfessor action

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,54 @@
 // this is for testing the complete process of looking professors up,
 // searching the relevent fields, and populating their information
 
-import {getProfessorNames} from "./mucatalog"
-import {getCoursesByProfessor} from "./mucourses"
-import {getArticleContentByName} from "./wikipedia" 
+import { getProfessorNames } from './mucatalog';
+import { getCoursesByProfessor } from './mucourses';
+import { getArticleContentByName } from './wikipedia';
 
-import { Professor } from "./models/professor"
+import { BasicInfo, ObjectiveMetrics, Professor } from './models/professor';
+import { Name } from './models/name';
+import { getCourses } from './mucourses';
+import { mucoursesData } from './mucourses';
 
-const TESTING = true
-async function main(){
-
-    // get professor names 
-    let allProfessorNames = await getProfessorNames()
-    // if testing, make names a subset 
-    if (TESTING){
-        allProfessorNames = allProfessorNames.slice(0,20)
+const TESTING = true;
+async function main() {
+    // get professor names
+    let allProfessorNames = await getProfessorNames();
+    // if testing, make names a subset
+    if (TESTING) {
+        allProfessorNames = allProfessorNames.slice(0, 20);
     }
 
+    // apply name analysis
+    allProfessorNames.map(onProfessorName);
+
+}
+
+export async function onProfessorName(name: Name): Promise<Professor> {
+
+    // TODO these things 
+    // check if professor exists
+    // if exists, get professor record and update
+    // if not exists, make professor id and new professor object
+
     // get mucourses data
+    const allCourses = await getCourses()
+    if (!allCourses){
+        throw new Error("Error with getCourses()")
+    }
+    const courses = getCoursesByProfessor(name, allCourses)
+    const totalCourses = courses.length
 
-    // apply wikipedia search
+    const totalGPA = courses.reduce((sum: number, course:mucoursesData) => {
+        console.log(course.average)
+        return sum+course.average
+    }, 0)
+    const averageGPA = Math.round((totalGPA/totalCourses)*100)/100
 
-    // write to database 
+
+    const basicInfo = new BasicInfo(name , "CS")
+    const objectiveMetrics = new ObjectiveMetrics(averageGPA, 0)
+
+    // make professor object (needs changing when database module complete)
+    return new Professor("testing", {basicInfo, objectiveMetrics})
 }
