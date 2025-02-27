3a781bf495c603fb443ca3daad804c95a7303fa6
jme14 <williamsltrey3@gmail.com>
2025-02-27 10:06:21 -0600

    fix wikipedia module to conform to name class

--- test/wikipedia.test.ts
+++ test/wikipedia.test.ts
@@ -1,15 +1,17 @@
 
 import {describe, expect, test} from "vitest"
 import {getArticleContentByName} from "../src/wikipedia"
-
+import { Name } from "../src/models/name"
 describe("Checking api call works", () => {
     test("Does it return something interesting for someone that has a wikipedia article", async () => {
-        const articleText = await getArticleContentByName("Jeffrey", "Uhlmann")
+        const jeff = new Name("Jeffrey", "Uhlmann")
+        const articleText = await getArticleContentByName(jeff)
         expect(articleText).toBeDefined()
     })
 
     test("Does it return nothing if there's no valid article", async () => {
-        const articleText = await getArticleContentByName("Jim", "Ries")
+        const jim = new Name("Jim", "Ries")
+        const articleText = await getArticleContentByName(jim)
         expect(articleText).toBeDefined()
         expect(articleText).toBe("No article")
     })
