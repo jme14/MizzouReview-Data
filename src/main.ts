3731c4e0514cc3d42b170a80ed1937e198e949df
jme14 <williamsltrey3@gmail.com>
2025-03-05 10:26:12 -0600

    now argument passed to main about testing

--- src/main.ts
+++ src/main.ts
@@ -5,57 +5,67 @@ import { getProfessorNames } from './mucatalog';
 import { getCoursesByProfessor } from './mucourses';
 import { getArticleContentByName } from './wikipedia';
 
-import { BasicInfo, ObjectiveMetrics, Professor, SubjectiveMetrics, AIPromptAnswers } from './models/professor';
+import {
+    BasicInfo,
+    ObjectiveMetrics,
+    Professor,
+    SubjectiveMetrics,
+    AIPromptAnswers,
+} from './models/professor';
 import { Name } from './models/name';
 import { getCourses } from './mucourses';
 import { mucoursesData } from './mucourses';
 
-main()
+main();
 async function main() {
+    console.log(process.argv.slice(2));
     // get professor names
     let allProfessorNames = await getProfessorNames();
     // if testing, make names a subset
-    console.log(process.env.NODE_ENV)
-    if (process.env.NODE_ENV == "development") {
+    if ( process.argv.slice(2)[0] == 'testing') {
         allProfessorNames = allProfessorNames.slice(0, 20);
     }
 
-    const allProfessors = allProfessorNames.map(async (name) => await onProfessorName(name));
-    allProfessors.forEach((prof) => console.log(prof))
+    const allProfessors = allProfessorNames.map(
+        async (name) => await onProfessorName(name),
+    );
+    allProfessors.forEach((prof) => console.log(prof));
 }
 
-// the data analysis based on the professor name 
+// the data analysis based on the professor name
 export async function onProfessorName(name: Name): Promise<Professor> {
-
-    // TODO these things 
+    // TODO these things
     // check if professor exists
     // if exists, get professor record and update
     // if not exists, make professor id and new professor object
 
     // get mucourses data
-    const allCourses = await getCourses()
-    if (!allCourses){
-        throw new Error("Error with getCourses()")
+    const allCourses = await getCourses();
+    if (!allCourses) {
+        throw new Error('Error with getCourses()');
     }
-    const courses = getCoursesByProfessor(name, allCourses)
-    const totalCourses = courses.length
+    const courses = getCoursesByProfessor(name, allCourses);
+    const totalCourses = courses.length;
 
-    const totalGPA = courses.reduce((sum: number, course:mucoursesData) => {
-        console.log(course.average)
-        return sum+course.average
-    }, 0)
-    const averageGPA = Math.round((totalGPA/totalCourses)*100)/100
+    const totalGPA = courses.reduce((sum: number, course: mucoursesData) => {
+        console.log(course.average);
+        return sum + course.average;
+    }, 0);
+    const averageGPA = Math.round((totalGPA / totalCourses) * 100) / 100;
 
-    let articleContent = await getArticleContentByName(name)
-    if (articleContent != "No Article"){
-        articleContent = "Article!"
+    let articleContent = await getArticleContentByName(name);
+    if (articleContent != 'No Article') {
+        articleContent = 'Article!';
     }
 
-
-    const basicInfo = new BasicInfo(name , "CS")
-    const objectiveMetrics = new ObjectiveMetrics(averageGPA, 0)
-    const funFacts = new AIPromptAnswers({funFacts: articleContent})
+    const basicInfo = new BasicInfo(name, 'CS');
+    const objectiveMetrics = new ObjectiveMetrics(averageGPA, 0);
+    const funFacts = new AIPromptAnswers({ funFacts: articleContent });
 
     // make professor object (needs changing when database module complete)
-    return new Professor("testing", {basicInfo: basicInfo, objectiveMetrics: objectiveMetrics, aIPromptAnswers: funFacts})
+    return new Professor('testing', {
+        basicInfo: basicInfo,
+        objectiveMetrics: objectiveMetrics,
+        aIPromptAnswers: funFacts,
+    });
 }
