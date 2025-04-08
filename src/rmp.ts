4f992c2aa8b5e1907dec58e0c57b781258c06a01
jme14 <williamsltrey3@gmail.com>
2025-04-08 09:39:37 -0500

    refactor to test easier different components of scraping

--- src/rmp.ts
+++ src/rmp.ts
@@ -1,22 +1,52 @@
-import {chromium} from 'playwright'
+import {chromium, Page} from 'playwright'
 
-export async function readWebsite(){
+async function sleep(seconds: number){
+    await new Promise(resolve => setTimeout(resolve, seconds*1000))
+}
+// go to RMP
+export async function getPage(){
     const browser = await chromium.launch({headless:false})
+    // const browser = await chromium.launch()
     const page = await browser.newPage()
     console.log("Awaiting page load")
-    page.goto("https://www.ratemyprofessors.com/search/professors/", {waitUntil:"load", timeout:0})
-    console.log("Done!")
+    // await page.goto("https://www.ratemyprofessors.com/search/professors/", {waitUntil:"load", timeout:0})
+    await page.goto("https://www.ratemyprofessors.com/search/professors/", {waitUntil: "domcontentloaded"})
+    return page
+}
+// fill the Professor search item 
+export async function fillProfName(page: Page){
+    const inputElements = page.getByPlaceholder("Professor name")
+    console.log(inputElements.allTextContents())
+    return true
+}
+export async function readWebsite(){
 
+    const page = await getPage() 
+
+    /*
+    const elementToClose = page.getByText("Close")
+    console.log(elementToClose)
     await page.getByText("Close").click()
+    */
     
     const inputLocator = page.locator("input")
     const inputCount = await inputLocator.count() 
+    console.log(`There are ${inputCount} inputs`)
+    await sleep(2)
+
+    /* 
+    console.log("Waiting starts...")
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    console.log("Waiting complete!")
+    */ 
 
     let professorInput 
     let schoolElement
     for (let i = 0 ; i  < inputCount ; i++){
         const inputElement = inputLocator.nth(i)
         const placeholderText = await inputElement.getAttribute("placeholder")
+        console.log(`Looking at input element ${i}, with ${placeholderText}`)
+        await sleep(2)
         if (placeholderText === "Professor name"){
             professorInput = inputElement
             await inputElement.fill("Jim Ries")
@@ -31,6 +61,7 @@ export async function readWebsite(){
         return false
     }
     console.log("Filled in data!")
+    await sleep(2)
     await page.keyboard.press("Enter")
     console.log("Pressed enter!")
     const jimThings = page.getByText("Jim Ries")
