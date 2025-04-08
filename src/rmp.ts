645e3715ddd0168be74039dffe0326542350d218
jme14 <williamsltrey3@gmail.com>
2025-04-08 12:30:58 -0500

    suboptimal, but something, to navigate to a professor page

--- src/rmp.ts
+++ src/rmp.ts
@@ -1,45 +1,89 @@
-import {Browser, chromium, Page} from 'playwright'
-import { OperationCanceledException } from 'typescript'
+import { Browser, chromium, Page, Locator } from 'playwright';
+import { OperationCanceledException } from 'typescript';
 
-export async function sleep(seconds: number){
-    await new Promise(resolve => setTimeout(resolve, seconds*1000))
+export async function sleep(seconds: number) {
+    await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 // go to RMP
-export async function getPage(){
-    const browser = await chromium.launch({headless:false})
+export async function getPage() {
+    const browser = await chromium.launch({ headless: false });
     // const browser = await chromium.launch()
-    const page = await browser.newPage()
-    console.log("Awaiting page load")
+    const page = await browser.newPage();
+    console.log('Awaiting page load');
     // await page.goto("https://www.ratemyprofessors.com/search/professors/", {waitUntil:"load", timeout:0})
-    await page.goto("https://www.ratemyprofessors.com/search/professors/", {waitUntil: "domcontentloaded"})
-    return {browser, page}
+    await page.goto('https://www.ratemyprofessors.com/search/professors/', {
+        waitUntil: 'domcontentloaded',
+    });
+    return { browser, page };
 }
-// fill the Professor search item 
-export async function fillProfName(browser: Browser, page: Page, name: string){
-    const inputElements = page.getByPlaceholder("Professor name").filter({visible: true})
-    const totalInputs = await inputElements.all()
-    if (totalInputs.length > 1){
-        throw new OperationCanceledException()
+// fill the Professor search item
+export async function fillProfName(browser: Browser, page: Page, name: string) {
+    const inputElements = page
+        .getByPlaceholder('Professor name')
+        .filter({ visible: true });
+    const totalInputs = await inputElements.all();
+    if (totalInputs.length > 1) {
+        throw new OperationCanceledException();
     }
-    await inputElements.fill(name)
-    return inputElements
+    await inputElements.fill(name);
+    return inputElements;
 }
-// fill the school search item 
-export async function fillSchool(browser: Browser, page: Page){
-    const inputElements = page.getByPlaceholder("Your school").filter({visible: true})
-    const totalInputs = await inputElements.all()
-    if (totalInputs.length > 1){
-        throw new OperationCanceledException()
+// fill the school search item
+export async function fillSchool(browser: Browser, page: Page) {
+    const inputElements = page
+        .getByPlaceholder('Your school')
+        .filter({ visible: true });
+    const totalInputs = await inputElements.all();
+    if (totalInputs.length > 1) {
+        throw new OperationCanceledException();
     }
-    await inputElements.fill("University of Missouri - Columbia")
-    return inputElements
+    await inputElements.fill('University of Missouri - Columbia');
+    return inputElements;
 }
-export async function readWebsite(){
+export async function navigateToProfListPage(
+    browser: Browser,
+    page: Page,
+    inputElem: Locator,
+) {
+    const beforeURL = page.url();
+    await inputElem.focus();
+    await page.keyboard.press('Enter');
+    const afterURL = page.url();
+    if (beforeURL == afterURL) {
+        return false;
+    }
+    return true;
+}
 
-    const {browser, page} = await getPage() 
+export async function navigateToFirstProfPage(
+    browser: Browser,
+    page: Page,
+    name: string,
+) {
+    const nameregex = new RegExp(name.replace(/\s+/g, '\\s+'));
 
-    const profInputElem = await fillProfName(browser, page, "Jim Ries")
-    const schoolInputElem = await fillSchool(browser, page)
+    // this provides two elements 
+    const allNameElements = await page
+        .getByText(nameregex)
+        .all();
+
+    const initialURL = page.url()
+    for (let i = 0 ; i < allNameElements.length ; i++){
+        await allNameElements.at(i)?.click()
+        const finalURL = page.url()
+        if (initialURL != finalURL){
+            return true
+        }
+    }
+    return false
+}
+export async function readWebsite() {
+    const { browser, page } = await getPage();
+
+    const profInputElem = await fillProfName(browser, page, 'Jim Ries');
+    const schoolInputElem = await fillSchool(browser, page);
+    await schoolInputElem.focus();
+    await page.keyboard.press('Enter');
 
     /* 
     console.log("Filled in data!")
--- test/rmp.test.ts
+++ test/rmp.test.ts
@@ -1,10 +1,19 @@
 import { beforeAll, describe, expect, test } from 'vitest';
-import { getPage, fillProfName, fillSchool, sleep } from '../src/rmp';
-import { Browser, Page } from 'playwright';
+import {
+    getPage,
+    fillProfName,
+    fillSchool,
+    sleep,
+    navigateToProfListPage,
+    navigateToFirstProfPage 
+} from '../src/rmp';
+import { Browser, Page, Locator} from 'playwright';
 
 describe('reading a website!', () => {
     let browser: Browser;
     let page: Page;
+    let profInputElem: Locator
+    let schoolInputElem: Locator
 
     beforeAll(async () => {
         ({ browser, page } = await getPage());
@@ -15,14 +24,23 @@ describe('reading a website!', () => {
     });
 
     test('filling prof input', async () => {
-        const profInputElem = await fillProfName(browser, page, 'Jim Ries');
+        profInputElem = await fillProfName(browser, page, 'Jim Ries');
         expect(profInputElem).toBeTruthy();
-        await sleep(1);
     });
 
     test('filling school input', async () => {
-        const schoolInputElem = await fillSchool(browser, page);
+        schoolInputElem = await fillSchool(browser, page);
         expect(schoolInputElem).toBeTruthy();
-        await sleep(3);
     });
+
+    test('navigating to prof list page', async () => {
+        const result = await navigateToProfListPage(browser, page, profInputElem)
+        expect(result).toBeTruthy()
+    });
+
+    test('seeing how many jimrs there are', async () => {
+        await sleep(1)
+        const jimrCount = await navigateToFirstProfPage(browser, page, "Jim Ries")
+        expect(jimrCount).toBeTruthy
+    })
 });
