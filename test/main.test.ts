16a3fd966eb75b40a64725a879c1604557cd6afe
jme14 <williamsltrey3@gmail.com>
2025-04-15 11:41:17 -0500

    add test for writing professors from mufaculty info

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,16 +2,12 @@
 import {describe, expect, test} from "vitest"
 import { Name } from "mizzoureview-reading"
 import { BasicInfo } from "mizzoureview-reading"
-import { onProfessorBasicInfo} from "../src/main"
 import { getCourses } from "../src/mucourses"
+import {updateFaculty} from "../src/main"
 describe("onProfessorName", async () => {
-    test("Using my main man JimR", async () => {
-        const jimr = new Name("James", "Ries")
-        const allCourses = await getCourses()
-        const jimrInfo = new BasicInfo(jimr, "NEEDS IMPROVEMENT")
-        const jimrProfessor = await onProfessorBasicInfo(jimrInfo, allCourses)
-        console.log(jimrProfessor)
-        expect(jimrProfessor).toBeTruthy()
-        expect(jimrProfessor.objectiveMetrics?.gpa).toBeTruthy()
+    test("testing getting the professor objects", async () => {
+        const professorArray = await updateFaculty()
+        console.log(professorArray)
+        expect(professorArray.success).toBeTruthy()
     })
 })
