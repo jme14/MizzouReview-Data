b5a223767aa71c044e1ba2d7abf7344b71c2d572
jme14 <williamsltrey3@gmail.com>
2025-04-15 14:47:16 -0500

    better testing mucourses

--- test/mucourses.test.ts
+++ test/mucourses.test.ts
@@ -1,59 +1,71 @@
-import {describe, expect, test} from "vitest"
-import {mucoursesData, getCourses, getCoursesByProfessor} from "../src/mucourses"
-import { Name } from "mizzoureview-reading"
+import { describe, expect, test } from 'vitest';
+import {
+    mucoursesData,
+    getCourses,
+    getCoursesByProfessor,
+    getProfessorObjectiveMetrics,
+    setProfessorObjectiveMetrics,
+} from '../src/mucourses';
+import {
+    Professor,
+    BasicInfo,
+    ObjectiveMetrics,
+    Name,
+} from 'mizzoureview-reading';
 
-describe("API access", () => {
-    test("getCourses defined", async ()=> {
-        const data = await getCourses()
-        expect(data).toBeDefined()
-    })
+describe.skip('API access', () => {
+    test('getCourses defined', async () => {
+        const data = await getCourses();
+        expect(data).toBeDefined();
+    });
 
-    test("getCourses consistent object subset test ", async () => {
-        const data: Object[] = await getCourses()
-        
-        const keySet = new Set()
-        Object.keys(data[0]).forEach((key) => keySet.add(key))
-        
-        const dataSubset = data.slice(0,100)
+    test('getCourses consistent object subset test ', async () => {
+        const data: Object[] = await getCourses();
+
+        const keySet = new Set();
+        Object.keys(data[0]).forEach((key) => keySet.add(key));
+
+        const dataSubset = data.slice(0, 100);
         dataSubset.forEach((datapoint: Object) => {
             Object.keys(datapoint).forEach((key) => {
-                console.log(keySet)
-                console.log(key)
-                expect(keySet.has(key)).toBe(true)
-            })
-        })
-        console.log(keySet)
-    })
-})
-describe("API result filtering", async () => {
-    const allCourses = await getCourses()
-    test("getCourses by instructor name, known", async() => {
-        const name = new Name("Jill", "Moreland", ["Annette"])
-        const results: mucoursesData[] = getCoursesByProfessor(name, allCourses)
-        expect(results.length).toBeGreaterThan(0)
+                console.log(keySet);
+                console.log(key);
+                expect(keySet.has(key)).toBe(true);
+            });
+        });
+        console.log(keySet);
+    });
+});
+describe('API result handling', async () => {
+    const allCourses = await getCourses();
+    const professor = new Professor('00LOnCH6NUaxjSTq7JRk', {
+        basicInfo: new BasicInfo(
+            new Name('Mitchell', 'Goldman'),
+            'Medicine-Infectious Diseases',
+            { education: 'Doctor of Medicine' },
+        ),
+    });
+    let objectiveMetrics: ObjectiveMetrics
+    test('getCourses by instructor name, known', async () => {
+        const name = new Name('Jill', 'Moreland', ['Annette']);
+        const results: mucoursesData[] = getCoursesByProfessor(
+            name,
+            allCourses,
+        );
+        expect(results.length).toBeGreaterThan(0);
         //console.log(results)
+    });
+    test('Testing known professor', async () => {
+        objectiveMetrics = getProfessorObjectiveMetrics(allCourses, professor);
+        console.log(objectiveMetrics)
+        expect(objectiveMetrics).toBeTruthy()
+    });
+    test('Testing correct setting', async ()=>{
+        const professors = await setProfessorObjectiveMetrics([professor])
+        professor.objectiveMetrics = objectiveMetrics
+
+        const professorResult = professors[0]
+        expect(professorResult).toEqual(professor)
+        console.log(professorResult)
     })
-
-    /*
-    test("getCourses by instructor name, middle initial unknown", async() => {
-        const name = new Name("James", "Ries")
-        const results: mucoursesData[] = getCoursesByProfessor(name, allCourses)
-        expect(results.length).toBeGreaterThan(0)
-        expect(results.length).toBeLessThan(1000)
-        console.log(results)
-    })
-    test("getCourses by instructor name, middle name unknown", async() => {
-        const jillName = new Name("Jill", "Moreland")
-        const jillResults: mucoursesData[] = getCoursesByProfessor(jillName, allCourses)
-        expect(jillResults.length).toBeGreaterThan(0)
-        expect(jillResults.length).toBeLessThan(1000)
-
-        const mckenzieName = new Name("Gary", "McKenzie")
-        const mckenzieResults: mucoursesData[] = getCoursesByProfessor(mckenzieName, allCourses)
-        expect(mckenzieResults.length).toBeGreaterThan(0)
-        expect(mckenzieResults.length).toBeLessThan(1000)
-
-
-    })
-    */
-})
+});
