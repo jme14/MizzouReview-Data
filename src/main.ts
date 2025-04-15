818655ebf0534dd7bade65594c8b9b599566835f
jme14 <williamsltrey3@gmail.com>
2025-04-15 14:49:37 -0500

    write functions to update mucourses data

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import {
 } from 'firebase-admin/firestore';
 
 import { getProfessorBasicInfo } from './mucatalog';
-import { getCoursesByProfessor } from './mucourses';
+import { getCoursesByProfessor, setProfessorObjectiveMetrics } from './mucourses';
 import { getArticleContentByName } from './wikipedia';
 
 import {
@@ -23,6 +23,7 @@ import {
     AIPromptAnswers,
     Name,
     getAllProfessors,
+    getSomeProfessors
 } from 'mizzoureview-reading';
 
 import { getCourses } from './mucourses';
@@ -30,12 +31,12 @@ import { mucoursesData } from './mucourses';
 import { initializeProfessor, updateProfessor } from './database';
 
 import { writeProfessors } from './database';
+import { TESTING, PROF_READ_LIMIT } from "../keys/config.json"
 
 const firebaseConfig = {
     credential: cert(require('../keys/admin.json')),
 };
 
-const TESTING = true
 // below function from chatgpt with minor edits
 function generateProfessorId(
     basicInfo: BasicInfo,
@@ -71,60 +72,34 @@ function generateProfessorId(
     return result;
 }
 
-export async function updateFaculty() {
-    // getting database information
-    const app = initializeApp(firebaseConfig);
-    const db: Firestore = getFirestore(app);
-
-    // const allDatabaseProfessors = await getAllProfessors(db)
+// this scrapes mufaculty to get the information which creates professor objects 
+export async function createProfessorsFromCatalog(): Promise<Professor[]>{
     let professorBasicInfo = await getProfessorBasicInfo();
     const professorArray: Professor[] = []
     professorBasicInfo.forEach((basicInfo) => {
         const newProf = new Professor(generateProfessorId(basicInfo), { basicInfo: basicInfo });
         professorArray.push(newProf)
     });
-
-    // if testing, make this smaller
-    if (TESTING){
-        professorBasicInfo = professorBasicInfo.slice(0,1000)
-    }
-
-    return await writeProfessors(db, professorArray)
+    return professorArray
 }
 
-// mainWithDatabase()
-// program flow including the database, updating records on matches
-/*
-async function mainWithDatabase(){
-
-    // getting database information 
+// this function scrapes mucatalog, then puts the data in the database
+export async function updateMUCatalog() {
+    // getting database information
     const app = initializeApp(firebaseConfig);
     const db: Firestore = getFirestore(app);
 
-    // TODO: run these at the same time 
-    const allDatabaseProfessors = await getAllProfessors(db)
-    const allProfessorObjects = await extractProfessorData()
-
-    const pInDB: Professor[] = []
-    const pNotInDB: Professor[] = []
-
-    // this creates two arrays based off if the professor is already in the database 
-    const [professorsInDatabase, professorsNotInDatabase] = allProfessorObjects.reduce((profs, prof) => {
-        // this returns true on prof existing in array and false on not  
-        const inDatabase = allDatabaseProfessors.some((dbProf) => {
-            if (!dbProf.basicInfo || !prof.basicInfo){
-                return false
-            } 
-            return dbProf.basicInfo.name == prof.basicInfo.name
-        })
-        // this pushes to first array if it's in database and second array if it isn't in the database 
-        profs[inDatabase ? 0 : 1].push(prof)
-        return profs
-    }, [pInDB, pNotInDB])
-
-    professorsNotInDatabase.forEach((prof) => initializeProfessor(db, prof))
-    professorsInDatabase.forEach(((prof) => updateProfessor(db, prof)))
-    console.log("Done?")
-    return true 
+    const professorArray = await createProfessorsFromCatalog()
+    return await writeProfessors(db, professorArray)
 }
-*/
+
+// this function accesses data from mucatalog, then puts data in the database 
+export async function updateMUCourses() {
+    // getting database information
+    const app = initializeApp(firebaseConfig);
+    const db: Firestore = getFirestore(app);
+
+    const professorArray = TESTING ? await getSomeProfessors(db, PROF_READ_LIMIT) : await getAllProfessors(db)
+    const updatedProfessorArray = await setProfessorObjectiveMetrics(professorArray)
+    return updatedProfessorArray
+}
