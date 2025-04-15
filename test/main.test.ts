673f71000585728dc2a3cb542854175e76d681aa
jme14 <williamsltrey3@gmail.com>
2025-04-15 14:47:41 -0500

    testing mucourses update

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,13 +1,25 @@
+import { describe, expect, test } from 'vitest';
+import { Name } from 'mizzoureview-reading';
+import { BasicInfo } from 'mizzoureview-reading';
+import { getCourses } from '../src/mucourses';
+import { updateMUCatalog, updateMUCourses } from '../src/main';
+import { TESTING, PROF_READ_LIMIT } from '../keys/config.json';
 
-import {describe, expect, test} from "vitest"
-import { Name } from "mizzoureview-reading"
-import { BasicInfo } from "mizzoureview-reading"
-import { getCourses } from "../src/mucourses"
-import {updateFaculty} from "../src/main"
-describe("onProfessorName", async () => {
-    test("testing getting the professor objects", async () => {
-        const professorArray = await updateFaculty()
-        console.log(professorArray)
-        expect(professorArray.success).toBeTruthy()
+describe('onProfessorName', async () => {
+    test('updateMUCatalog()', async () => {
+        // don't run if we don't want to write a billion things
+        if (TESTING) {
+            return;
+        }
+
+        const professorArray = await updateMUCatalog();
+        console.log(professorArray);
+        expect(professorArray.success).toBeTruthy();
+    });
+    test('updateMUCourses()', async() => {
+        if (TESTING){
+            const professors = await updateMUCourses()
+            expect(professors).toBe(PROF_READ_LIMIT)
+        }
     })
-})
+});
