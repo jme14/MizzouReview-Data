4cb087890718d48e922cfac01477be3918766c7c
jme14 <williamsltrey3@gmail.com>
2025-04-15 11:40:52 -0500

    add batch writing for more efficient writing to db

--- src/database.ts
+++ src/database.ts
@@ -6,6 +6,7 @@ import {
     Filter,
     CollectionReference,
     DocumentReference,
+    WriteBatch
 } from 'firebase-admin/firestore';
 
 import {
@@ -22,6 +23,58 @@ type Result<T> = {
     message: string;
     data?: T;
 };
+
+async function batchWriteProfessors(db: Firestore, professorArray: Professor[]): Promise<Result<Professor[]>>{
+    if (professorArray.length > 500){
+        throw new Error("Split this array before using this function; this array is too large")
+    }
+
+    function batchSet(prof: Professor){
+        const docRef = db.collection("professors").doc(prof.professorId)
+        const profData = JSON.parse(JSON.stringify(prof)) 
+        batch.set(docRef, profData)
+    }
+
+    const batch: WriteBatch = db.batch()
+    professorArray.forEach(batchSet)
+
+    await batch.commit()
+    return {
+        success: true,
+        message: "Write successful",
+        data: professorArray
+    }
+}
+// this is the exported function, should handle all problems in here
+export async function writeProfessors(db: Firestore, professorArray: Professor[]){
+
+    // batch calls to firebase are in groups of 500, so split into these groups 
+    const professorSubArrays: Professor[][] = []
+    const maxArraySize = 500
+    for ( let i = 0 ; i < professorArray.length ; i=i+maxArraySize){
+        professorSubArrays.push(professorArray.slice(i,i+maxArraySize))
+    }
+
+    // hold the promises which resolve upon the batch operations completing
+    const promiseArray: Promise<Result<Professor[]>>[] = []
+    professorSubArrays.forEach((arr) => promiseArray.push(batchWriteProfessors(db, arr)))
+    
+    // wait for all the promises to complete 
+    const results = await Promise.allSettled(promiseArray)
+    const failedWrites = results.filter((result) => result.status === "rejected")
+    if (failedWrites.length > 0){
+        return {
+            success: false,
+            message: "Some batches failed",
+            data: results
+        }
+    }
+    return {
+        success: true,
+        message: "Writes successful",
+        data: results
+    }
+}
 
 // enable overwrite if you expect that the professor already exists in the database.
 /**
