3cae0a830c249b5e906be6bb1bfd10291f353b1d
jme14 <williamsltrey3@gmail.com>
2025-04-17 16:34:59 -0500

    change config method and imports

--- src/database.ts
+++ src/database.ts
@@ -6,80 +6,91 @@ import {
     Filter,
     CollectionReference,
     DocumentReference,
-    WriteBatch
+    WriteBatch,
 } from 'firebase-admin/firestore';
 
+import { Professor } from 'mizzoureview-reading/models/professor';
 import {
-    Professor, Name
-} from "mizzoureview-reading"
-
-import {
-   getAllProfessors,
-   getProfessorFromID,
-   getProfessorsFromDepartment 
-} from "mizzoureview-reading"
+    getAllProfessors,
+    getProfessorFromID,
+    getProfessorsFromDepartment,
+} from 'mizzoureview-reading/database-admin';
 type Result<T> = {
     success: boolean;
     message: string;
     data?: T;
 };
 
-import { TESTING } from "../keys/config.json"
+import config from '../keys/config.json' with { type: "json"};
+const TESTING = config.TESTING
 
-async function batchWriteProfessors(db: Firestore, professorArray: Professor[]): Promise<Result<Professor[]>>{
-    if (professorArray.length > 500){
-        throw new Error("Split this array before using this function; this array is too large")
+async function batchWriteProfessors(
+    db: Firestore,
+    professorArray: Professor[],
+): Promise<Result<Professor[]>> {
+    if (professorArray.length > 500) {
+        throw new Error(
+            'Split this array before using this function; this array is too large',
+        );
     }
 
-    function batchSet(prof: Professor){
-        const docRef = db.collection("professors").doc(prof.professorId)
-        const profData = JSON.parse(JSON.stringify(prof)) 
-        batch.set(docRef, profData)
+    function batchSet(prof: Professor) {
+        const docRef = db.collection('professors').doc(prof.professorId);
+        const profData = JSON.parse(JSON.stringify(prof));
+        batch.set(docRef, profData);
     }
 
-    const batch: WriteBatch = db.batch()
-    professorArray.forEach(batchSet)
+    const batch: WriteBatch = db.batch();
+    professorArray.forEach(batchSet);
 
-    await batch.commit()
+    await batch.commit();
     return {
         success: true,
-        message: "Write successful",
-        data: professorArray
-    }
+        message: 'Write successful',
+        data: professorArray,
+    };
 }
 // this is the exported function, should handle all problems in here
-export async function writeProfessors(db: Firestore, professorArray: Professor[]){
-    if (TESTING){
+export async function writeProfessors(
+    db: Firestore,
+    professorArray: Professor[],
+) {
+    if (TESTING) {
         return {
             success: false,
-            message: "Testing is turned on to not bankrupt TMNH, here's what would have been written",
-            data: professorArray
-        }
+            message:
+                "Testing is turned on to not bankrupt TMNH, here's what would have been written",
+            data: professorArray,
+        };
     }
-    // batch calls to firebase are in groups of 500, so split into these groups 
-    const professorSubArrays: Professor[][] = []
-    const maxArraySize = 500
-    for ( let i = 0 ; i < professorArray.length ; i=i+maxArraySize){
-        professorSubArrays.push(professorArray.slice(i,i+maxArraySize))
+    // batch calls to firebase are in groups of 500, so split into these groups
+    const professorSubArrays: Professor[][] = [];
+    const maxArraySize = 500;
+    for (let i = 0; i < professorArray.length; i = i + maxArraySize) {
+        professorSubArrays.push(professorArray.slice(i, i + maxArraySize));
     }
 
     // hold the promises which resolve upon the batch operations completing
-    const promiseArray: Promise<Result<Professor[]>>[] = []
-    professorSubArrays.forEach((arr) => promiseArray.push(batchWriteProfessors(db, arr)))
-    
-    // wait for all the promises to complete 
-    const results = await Promise.allSettled(promiseArray)
-    const failedWrites = results.filter((result) => result.status === "rejected")
-    if (failedWrites.length > 0){
+    const promiseArray: Promise<Result<Professor[]>>[] = [];
+    professorSubArrays.forEach((arr) =>
+        promiseArray.push(batchWriteProfessors(db, arr)),
+    );
+
+    // wait for all the promises to complete
+    const results = await Promise.allSettled(promiseArray);
+    const failedWrites = results.filter(
+        (result) => result.status === 'rejected',
+    );
+    if (failedWrites.length > 0) {
         return {
             success: false,
-            message: "Some batches failed",
-            data: results
-        }
+            message: 'Some batches failed',
+            data: results,
+        };
     }
     return {
         success: true,
-        message: "Writes successful",
-        data: results
-    }
+        message: 'Writes successful',
+        data: results,
+    };
 }
