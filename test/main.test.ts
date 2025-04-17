7bf63126d18b9d146ee0083991dbd1e6d8a5417e
jme14 <williamsltrey3@gmail.com>
2025-04-17 16:45:35 -0500

    fix imports for main tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,38 +1,43 @@
 import { describe, expect, test } from 'vitest';
-import { Name, Professor } from 'mizzoureview-reading';
-import { BasicInfo } from 'mizzoureview-reading';
-import { createProfessorsFromCatalog, updateMUCourses, writeRMP} from '../src/main';
-import { TESTING, PROF_READ_LIMIT } from '../keys/config.json';
+
+import { Professor } from 'mizzoureview-reading/models/professor';
+
+import {
+    createProfessorsFromCatalog,
+    updateMUCourses,
+    writeRMP,
+} from '../src/main';
 
 describe.skip('professor object management', () => {
-    let mucatalogProfessorArray: Professor[]
-    let mucoursesProfessorArray: Professor[]
+    let mucatalogProfessorArray: Professor[];
+    let mucoursesProfessorArray: Professor[];
 
     test('createProfessorsFromCatalog()', async () => {
         mucatalogProfessorArray = await createProfessorsFromCatalog();
         expect(mucatalogProfessorArray).toBeTruthy();
-        expect(mucatalogProfessorArray.length).toBeGreaterThan(0)
+        expect(mucatalogProfessorArray.length).toBeGreaterThan(0);
     });
 
-    test('updateMUCourses()', async() => {
-        mucoursesProfessorArray = mucatalogProfessorArray
+    test('updateMUCourses()', async () => {
+        mucoursesProfessorArray = mucatalogProfessorArray;
 
         // deep copy to keep track of how array has changed
-        mucatalogProfessorArray = mucatalogProfessorArray.map((prof) => ({...prof}))
+        mucatalogProfessorArray = mucatalogProfessorArray.map((prof) => ({
+            ...prof,
+        }));
 
-        const mucoursesSuccess = await updateMUCourses(mucoursesProfessorArray)
+        const mucoursesSuccess = await updateMUCourses(mucoursesProfessorArray);
 
-        // no elements should be lost 
-        expect(mucoursesSuccess).toBeTruthy()
+        // no elements should be lost
+        expect(mucoursesSuccess).toBeTruthy();
 
         // the arrays should not be the same
-        expect(mucatalogProfessorArray).not.toBe(mucoursesProfessorArray)
-    })
-
+        expect(mucatalogProfessorArray).not.toBe(mucoursesProfessorArray);
+    });
 });
 
 describe('writing to database functions', () => {
-    test('writeRMP', {timeout:6_000_0000}, async () => {
-        await writeRMP()
-    })
-})
+    test('writeRMP', { timeout: 6_000_0000 }, async () => {
+        await writeRMP();
+    });
+});
