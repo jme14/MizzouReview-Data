5c44813f69b70ed5f990609b0c165da19689b4f4
jme14 <williamsltrey3@gmail.com>
2025-04-17 16:48:00 -0500

    fix imports, formatting

--- test/wikipedia.test.ts
+++ test/wikipedia.test.ts
@@ -1,19 +1,18 @@
+import { describe, expect, test } from 'vitest';
+import { getArticleContentByName } from '../src/wikipedia';
+import { Name } from 'mizzoureview-reading/models/name';
 
-import {describe, expect, test} from "vitest"
-import {getArticleContentByName} from "../src/wikipedia"
-import { Name } from "mizzoureview-reading"
-
-describe("Checking api call works", () => {
-    test("Does it return something interesting for someone that has a wikipedia article", async () => {
-        const jeff = new Name("Jeffrey", "Uhlmann")
-        const articleText = await getArticleContentByName(jeff)
-        expect(articleText).toBeDefined()
-    })
+describe('Checking api call works', () => {
+    test('Does it return something interesting for someone that has a wikipedia article', async () => {
+        const jeff = new Name('Jeffrey', 'Uhlmann');
+        const articleText = await getArticleContentByName(jeff);
+        expect(articleText).toBeDefined();
+    });
 
     test("Does it return nothing if there's no valid article", async () => {
-        const jim = new Name("Jim", "Ries")
-        const articleText = await getArticleContentByName(jim)
-        expect(articleText).toBeDefined()
-        expect(articleText).toBe("No article")
-    })
-})
+        const jim = new Name('Jim', 'Ries');
+        const articleText = await getArticleContentByName(jim);
+        expect(articleText).toBeDefined();
+        expect(articleText).toBe('No article');
+    });
+});
