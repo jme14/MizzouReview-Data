0c75e5dedb85e1db47aec6d8fd806ea540f25fe8
jme14 <williamsltrey3@gmail.com>
2025-04-17 16:46:17 -0500

    fix imports

--- test/mucatalog.test.ts
+++ test/mucatalog.test.ts
@@ -1,27 +1,26 @@
+import { describe, expect, test } from 'vitest';
+import { BasicInfo } from 'mizzoureview-reading/models/professor';
+import { getProfessorBasicInfo } from '../src/mucatalog';
 
-import {describe, expect, test} from "vitest"
-import { BasicInfo } from "mizzoureview-reading"
-import {getProfessorBasicInfo} from "../src/mucatalog" 
-
-describe("getProfessorBasicInfo", async () => {
+describe('getProfessorBasicInfo', async () => {
     let bI: BasicInfo[];
 
-    test("Truthy", async () =>{
-        bI = await getProfessorBasicInfo()
-        console.log(bI)
-        expect(bI).toBeTruthy()
-    })
-    test("Not length 0", () =>{
-        expect(bI.length).toBeGreaterThan(0)
-    })
-    test("Length reasonable guess", () =>{
-        expect(bI.length).toBeGreaterThan(300)
-    })
-    test("Basic info not formatted stupid", () =>{
+    test('Truthy', async () => {
+        bI = await getProfessorBasicInfo();
+        console.log(bI);
+        expect(bI).toBeTruthy();
+    });
+    test('Not length 0', () => {
+        expect(bI.length).toBeGreaterThan(0);
+    });
+    test('Length reasonable guess', () => {
+        expect(bI.length).toBeGreaterThan(300);
+    });
+    test('Basic info not formatted stupid', () => {
         bI.forEach((basicInfo) => {
-            expect(basicInfo.department.trim()).toBe(basicInfo.department)
-            expect(basicInfo.education?.trim()).toBe(basicInfo.education)
-            expect(basicInfo.title?.trim()).toBe(basicInfo.title)
-        })
-    })
-})
+            expect(basicInfo.department.trim()).toBe(basicInfo.department);
+            expect(basicInfo.education?.trim()).toBe(basicInfo.education);
+            expect(basicInfo.title?.trim()).toBe(basicInfo.title);
+        });
+    });
+});
