1c78621aeadd9e05b131391f4cb4e3135b15de58
jme14 <williamsltrey3@gmail.com>
2025-04-18 18:57:06 -0500

    add rmp module to writeOptions, as well as improved writeRMP

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ There are two major types of functions here
 
 There's also the object creator function, which obtains the list of names in the first place
 */
-import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { initializeApp, applicationDefault, cert, ServiceAccount } from 'firebase-admin/app';
 import {
     getFirestore,
     Firestore,
@@ -21,14 +21,16 @@ import {
     DocumentReference,
 } from 'firebase-admin/firestore';
 
+import cliProgress from "cli-progress"
+
 import { Professor } from 'mizzoureview-reading/models/professor';
 import {
     getAllProfessors,
     getSomeProfessors,
 } from 'mizzoureview-reading/database-admin';
 
-import config from '../keys/config.json' with {type: "json"}
-const { TESTING, PROF_READ_LIMIT, RMP_ARRAY_LIMIT} = config
+import mrdConfig from '../keys/config.json' with {type: "json"}
+const { TESTING, PROF_READ_LIMIT, RMP_ARRAY_LIMIT} = mrdConfig
 
 import { getProfessorBasicInfo } from './mucatalog.js';
 import {
@@ -37,7 +39,8 @@ import {
 } from './mucourses.js';
 import { generateProfessorId } from './professorId.js';
 import { writeProfessors } from './database.js';
-import { getPage, setProfessorSubjectiveMetricsLimited } from './rmp.js';
+import { getPage, setProfessorSubjectiveMetricsLimited, sleep } from './rmp.js';
+import { Browser, Page } from 'playwright';
 
 type WriteOptions = {
     mucatalog?: boolean;
@@ -45,8 +48,24 @@ type WriteOptions = {
     wikipedia?: boolean;
     rmp?: boolean;
 };
+
+import {config} from 'dotenv'
+config()
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+} as ServiceAccount;
 const firebaseConfig = {
-    credential: cert(require('../keys/admin.json')),
+    credential: cert(serviceAccount)
 };
 
 // this scrapes mufaculty to get the information which creates professor objects
@@ -110,15 +129,93 @@ export async function writeMUCourses() {
 
 /* this rmp section is going to be kinda stupid */
 
-export async function writeRMP() {
-    const { db, professorArray } = await initializeProfessorArrayFromDB();
-
-    const filteredProfessorArray = professorArray.filter(
+export function filterProfessorsForRMP(professors: Professor[], forceUpdateAllRecords: Boolean){
+    const allValidProfessors = professors.filter(
         (professor) =>
+            // they must have a basic info
             professor.basicInfo &&
+            // they must have objective metrics 
             professor.objectiveMetrics &&
-            professor.objectiveMetrics.gpa != 0,
+            // they must have a gpa (suggests they teach classes)
+            professor.objectiveMetrics.gpa != 0 
+            // they must not have subjective metrics initialized
     );
+    if (forceUpdateAllRecords){
+        return allValidProfessors 
+    }
+    const professorsSubjectiveUninitialized = allValidProfessors.filter(prof => !prof.subjectiveMetrics) 
+    return professorsSubjectiveUninitialized
+
+}
+
+class SearchRMPInvalidParamsError extends Error {
+    constructor(message: string){
+        super(message)
+    }
+}
+class SearchRMPCrashError extends Error {
+    constructor(message: string){
+        super(message)
+    }
+}
+export async function searchRMP(oldBrowser: Browser, oldPage: Page, shortProfessors: Professor[], retryCounter: number){
+    let success: Boolean = true 
+    try {
+        success = await setProfessorSubjectiveMetricsLimited(
+            oldBrowser,
+            oldPage,
+            shortProfessors
+        );
+        if (!success){
+            throw new SearchRMPInvalidParamsError("Params invalid in searchRMP")
+        }
+    } catch (err){
+        // if params error, throw it upward 
+        if (err instanceof SearchRMPInvalidParamsError){
+            throw(err)
+        }
+        
+        // otherwise, something unintended went wrong with the browser, and we need to restart 
+
+        await oldBrowser.close()
+        if (retryCounter > 0){
+
+            // exponential backoff 
+            await sleep(27/(retryCounter*retryCounter))
+            const {browser, page} = await getPage()
+            return searchRMP(browser, page, shortProfessors, retryCounter-1)
+
+        }
+        throw new SearchRMPCrashError("Retried and cannot load content.")
+    }
+}
+
+export interface WriteRMPOptions{
+    db?: Firestore,
+    professorArray?: Professor[],
+    forceUpdateRecords?: boolean 
+}
+export async function writeRMP(options?: WriteRMPOptions){ 
+
+    if (options == undefined){
+        options = {
+            db: undefined,
+            professorArray: undefined,
+            forceUpdateRecords: false
+        }
+    }
+
+    let db: Firestore | undefined = (options.db != undefined) ? options.db : undefined 
+    let professorArray: Professor[] | undefined  = options.professorArray
+    let forceUpdateRecords = options.forceUpdateRecords == undefined ? false : options.forceUpdateRecords
+
+    if (!db && !professorArray){
+        ({db, professorArray} = await initializeProfessorArrayFromDB())
+    }
+    if (!db || !professorArray){
+        return false
+    }
+    const filteredProfessorArray = filterProfessorsForRMP(professorArray, forceUpdateRecords)
     let professorSubarrays: Professor[][] = [];
 
     for (let i = 0; i < filteredProfessorArray.length; i += RMP_ARRAY_LIMIT) {
@@ -129,14 +226,47 @@ export async function writeRMP() {
 
     const { browser, page } = await getPage();
 
+    let fatalErrorCounter = 0
+    let lastRunFatalError = false 
+
+    // create a new progress bar instance
+    const progressBar = new cliProgress.SingleBar({
+        format: 'Progress |{bar}| {percentage}% || {value}/{total} items',
+        barCompleteChar: '\u2588',
+        barIncompleteChar: '\u2591',
+        hideCursor: true
+    });
+    progressBar.start(professorSubarrays.length, 0)
     for (let i = 0; i < professorSubarrays.length; i++) {
-        await setProfessorSubjectiveMetricsLimited(
-            browser,
-            page,
-            professorSubarrays[i],
-        );
+        try{
+            await searchRMP(
+                browser,
+                page,
+                professorSubarrays[i],
+                3
+            );
+
+        }catch(err){
+            if (err instanceof SearchRMPCrashError){
+                if (lastRunFatalError){
+                    console.log("TERMINATING SEARCH, TOO MANY FAILURES")
+                    return false
+                }
+                console.log("WARNING: FATAL ERROR, IF THIS HAPPENS AGAIN I WILL TERMINATE THE PROGRAM")
+                fatalErrorCounter++
+                lastRunFatalError = true 
+            } else if (err instanceof SearchRMPInvalidParamsError){
+                console.log("The following array has invalid data:")
+                professorSubarrays[i].forEach(prof => console.log(prof))
+            }
+            // only write professors if everything went okay
+            continue
+        }finally{
+            progressBar.update(i+1)
+        }    
         await writeProfessors(db, professorSubarrays[i]);
     }
+    progressBar.stop()
     return true;
 }
 
@@ -169,6 +299,16 @@ export async function writeOptions(options: WriteOptions) {
         if (!mucoursesSuccess) {
             throw new Error('Failure to update with mucourses data');
         }
+    }
+
+    if (options.rmp){
+        const writeRMPSuccess = await writeRMP({db: db, professorArray: professorArray})
+        if (writeRMPSuccess){
+            console.log("Success writing RMP data!")
+        } else {
+            console.log("Failure writing RMP data...")
+        }
+        return
     }
 
     return await writeProfessors(db, professorArray);
